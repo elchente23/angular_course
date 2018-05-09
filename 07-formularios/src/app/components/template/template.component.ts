5af7c666d1d34414ca875b1bb903002e9edf0ef4
Juan Vicente <elchente23@gmail.com>
2018-05-08 23:59:23 -0600

    Se terminó la clase de formularios sencillos

--- 07-formularios/src/app/components/template/template.component.ts
+++ 07-formularios/src/app/components/template/template.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-template',
@@ -7,13 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TemplateComponent implements OnInit {
 
+  usuario: object = {
+    nombre: null,
+    apellido: null,
+    email: null
+  };
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  guardar() {
-    console.log("Server side");
+  guardar(forma: NgForm) {
+    console.log("ngForm", forma);
+    console.log("valor", forma.value);
+    console.log("usuario", this.usuario);
   }
 
 }
