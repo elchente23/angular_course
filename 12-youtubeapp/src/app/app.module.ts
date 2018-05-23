75d1c0e0dd03c62ce7f8196a6251d6b2b62c83d0
Juan Vicente Barron Amador <juan.barron@neoris.cxnetworks.net>
2018-05-23 14:35:35 -0600

    Se creo el proyecto de youtubeapp

---
+++ 12-youtubeapp/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { YoutubeService } from './services/youtube.service';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [
+    YoutubeService
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
